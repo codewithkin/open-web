b89ddb4baf6a943169d5e5ee33b57207df435b90
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 18:24:46 +0200

    [ADD]: SEO metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="OpenConfess" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
