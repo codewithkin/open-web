8b88677cc1467f192f83c44f88ea105c28b706ac
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 18:50:59 +0200

    [UPDATE]: Empty confession styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { BrushCleaning } from "lucide-react";
 import Link from "next/link";
 
@@ -5,16 +6,18 @@ function Feed() {
   const confessions = [];
 
   return (
-    <section className="w-full min-h-screen">
+    <section className="w-full min-h-screen p-4">
       {
         confessions.length > 0 ? (
           <article></article>
         ) : (
-          <article className="w-full h-full flex flex-col justify-center items-center text-center md:py-20 py-12 px-4">
-            <BrushCleaning className="text-gray-400 dark:text-gray-200" size={96} strokeWidth={1.2} />
-            <h2 className="text-3xl md:text-2xl font-bold">Ooops</h2>
-            <p className="text-center text-gray-400 dark:text-gray-200 md:text-md">No confessions for today, please check again tomorrow or <Link className="underline text-green-500 dark:text-green-300" href="/confessions/new">post one</Link></p>
-          </article>
+          <Card className="w-full h-full flex flex-col justify-center items-center text-center md:my-20 my-12">
+            <CardContent className="w-full h-full flex flex-col justify-center items-center text-center">
+              <BrushCleaning className="text-gray-400 dark:text-gray-200" size={96} strokeWidth={1.2} />
+              <h2 className="text-3xl md:text-2xl font-bold">Ooops</h2>
+              <p className="text-center text-gray-400 dark:text-gray-200 md:text-md">No confessions for today, please check again tomorrow or <Link className="underline text-green-500 dark:text-green-300" href="/confessions/new">post one</Link></p>
+            </CardContent>
+          </Card>
         )
       }
     </section>
