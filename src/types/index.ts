e0cba099800701f457b4fb587a76e54301e6886e
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 20:20:42 +0200

    [ADD]: Types

---
+++ src/types/index.ts
@@ -0,0 +1,44 @@
+export interface Confession {
+    id: string;
+    text?: string;
+    audioUrl?: string;
+    creatorName: string;
+    likes: ConfessionLike[];
+    comments: ConfessionComment[];
+    postedAt: string; // ISO date string
+}
+
+export interface ConfessionLike {
+    id: string;
+    creatorName: string;
+    confessionId: string;
+    confession: Confession;
+    createdAt: string;
+}
+
+export interface ConfessionComment {
+    id: string;
+    creatorName: string;
+    confessionId: string;
+    confession: Confession;
+    text: string;
+    createdAt: string;
+    comments: ConfessionCommentComment[];
+    likes: ConfessionCommentLike[];
+}
+
+export interface ConfessionCommentLike {
+    id: string;
+    creatorName: string;
+    commentId: string;
+    comment: ConfessionComment;
+    createdAt: string;
+}
+
+export interface ConfessionCommentComment {
+    id: string;
+    creatorName: string;
+    commentId: string;
+    comment: ConfessionComment;
+    createdAt: string;
+}
